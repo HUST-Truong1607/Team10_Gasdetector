e19b83b04e0bd27c1f24d7c77128e4ae86e6a742
HUST-Truong1607 <truonghd1607@gmail.com>
2025-01-16 07:01:06 +0700

    GasDetector.ino

--- GasDetector.ino
+++ GasDetector.ino
@@ -1,7 +1,7 @@
 // C++ code
 //
 #include <SoftwareSerial.h>
-SoftwareSerial mySerial(10,9); //SIM800L Tx & Rx is connected to Arduino #3 & #2
+SoftwareSerial mySerial(11,10); //SIM800L Tx & Rx is connected to Arduino #3 & #2
 int led = 13;
 int buzzer = 7;
 double Gassensor;
@@ -11,37 +11,55 @@ void setup()
   pinMode(led, OUTPUT);
   pinMode(buzzer, OUTPUT);
   Serial.begin(9600);
-  mySerial.begin(9600);
-
 }
-void setupSIM()
-{
-  Serial.println("Initializing..."); 
-  delay(1000);
 
-  mySerial.println("AT"); //Once the handshake test is successful, it will back to OK
-  updateSerial();
+#define PHONE_NUMBER      "0965842465"              // thay đổi số điện thoại
+#define SET_BAUDRATE      "AT+IPREX = 115200"         // thay đổi badrate (VD: "AT+IPREX=115200")
+#define MCU_SIM_BAUDRATE  115200
 
-  mySerial.println("AT+CMGF=1"); // Configuring TEXT mode
-  updateSerial();
-  mySerial.println("AT+CMGS=\"+84961089406\"");
-  //change ZZ with country code and xxxxxxxxxxx with phone number to sms
-  updateSerial();
-  mySerial.print("Gas leaked"); //text content
-  updateSerial();
-  mySerial.write(26);
+void sim_at_wait(){
+    delay(100);
+    while (simSerial.available()) {
+        Serial.write(simSerial.read());
+    }
 }
-void updateSerial()
-{
-  delay(500);
-  while (Serial.available()) 
-  {
-    mySerial.write(Serial.read());//Forward what Serial received to Software Serial Port
-  }
-  while(mySerial.available()) 
-  {
-    Serial.write(mySerial.read());//Forward what Software Serial received to Serial Port
-  }
+
+bool sim_at_cmd(String cmd){
+    simSerial.println(cmd);
+    sim_at_wait();
+    return true;
+}
+
+bool sim_at_send(char c){
+    simSerial.write(c);
+    return true;
+}
+
+void sent_sms() {             // Hàm thực hiện gửi tin nhắn
+    sim_at_cmd("AT+CMGF=1");
+    String temp = "AT+CMGS=\"";
+    temp += (String)PHONE_NUMBER;
+    temp += "\"";
+    sim_at_cmd(temp);
+    sim_at_cmd("Gas leaked");
+    sim_at_send(0x1A);    // End charactor for SMS
+}
+void setupSIM() {
+    delay(200);
+    simSerial.begin(MCU_SIM_BAUDRATE);
+    delay(200);
+    
+    Serial.println("System started!!!!");
+    sim_at_cmd("AT");           // Kiểm tra AT Command
+    delay(200);
+    sim_at_cmd("AT+IPREX?");    // Kiểm tra tốc độ baud rate
+    delay(200);
+    sim_at_cmd(SET_BAUDRATE);   // Cài tốc độ baudrate
+    delay(200);
+    pinMode(13,OUTPUT); 
+    digitalWrite(13,HIGH);
+    sent_sms();                 // thực hiện gửi tin nhắn
+    delay(5000);                // Delay 5s 
 }
 
 void loop()
